af8284a45eb8a19c924d772ede676965a8847843
catpea <corporate.miou@gmail.com>
2023-10-01 17:11:58 -0400

    New Release

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import fs from 'fs-extra';
 import path from 'path';
 import ejs from 'ejs';
 import lodash from 'lodash';
+import { marked } from 'marked';
 import {execa} from 'execa';
 
 const moduleURL = new URL(import.meta.url);
@@ -14,7 +15,14 @@ const options = {
   closeDelimiter: '>',
 };
 
-let sets = (await fs.readdir('docs', {withFileTypes:true})).filter(dirent => dirent.isDirectory()).filter(dirent => !dirent.name.startsWith('_')).map(({name:location}) => ({ files: fs.readdirSync(path.join('docs', location), { withFileTypes: true }) .filter(dirent => dirent.isFile()) .filter(dirent => !dirent.name.startsWith('_')) .map(({name}) => path.join( location, name)) .map(o=>({
+function isImage(name){
+  return name.match(/jpg$|png$|bmp$/)
+}
+
+let sets = (await fs.readdir('docs', {withFileTypes:true})).filter(dirent => dirent.isDirectory()).filter(dirent => !dirent.name.startsWith('_')).map(({name:location}) => ({ id: location, text:(fs.existsSync(path.join('docs', location, 'README.md'))?  marked.parse(fs.readFileSync(path.join('docs', location, 'README.md')).toString()):'' ),   files: fs.readdirSync(path.join('docs', location), { withFileTypes: true }) .filter(dirent => dirent.isFile())
+ .filter(dirent => !dirent.name.startsWith('_'))
+ .filter(dirent => isImage(dirent.name))
+  .map(({name}) => path.join( location, name)) .map(o=>({
   url:o,
   thumbnail:path.join('preview', o.replace(/\.[a-z0-9]{3,4}$/,'.jpg')),
   image:path.join('docs', o),
@@ -25,7 +33,7 @@ let sets = (await fs.readdir('docs', {withFileTypes:true})).filter(dirent => dir
 for (const set of sets) {
   for (const file of set.files) {
     await fs.ensureDir(path.dirname(file.preview));
-    await execa('convert', [file.image, '-scale','25%', file.preview]);
+    await execa('convert', [file.image, '-resize','256x>', file.preview]);
   }
 }
 
